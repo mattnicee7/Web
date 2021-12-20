cc8cc723bfa11a30ddb2c43180c41cb5af69197a
Matheus Colin <mattxnice@gmail.com>
2021-12-20 00:50:11 -0300

    Order, Instant, ISO 8601 - PART 1

---
+++ src/main/java/com/github/mattnicee7/entities/Order.java
@@ -0,0 +1,43 @@
+package com.github.mattnicee7.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id.equals(order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
--- src/main/java/com/github/mattnicee7/entities/User.java
+++ src/main/java/com/github/mattnicee7/entities/User.java
@@ -2,27 +2,41 @@ package com.github.mattnicee7.entities;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @NoArgsConstructor
-@AllArgsConstructor
 @Getter
-@Setter
-@Entity(name = "users")
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
     private Long id;
+    @Setter
     private String name;
+    @Setter
     private String email;
+    @Setter
     private String phone;
+    @Setter
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public User(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
 
     @Override
     public boolean equals(Object o) {
